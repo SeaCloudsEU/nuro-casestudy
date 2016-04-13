ce61599bd90fad7a5b1488ccd710157a02ae4e55
Christian Tismer <christian.tismer@nurogames.com>
2016-04-13 20:27:40 +0200

    Clone of SVN Version 100

---
+++ Packages/nuro-api/nuro-casestudy/sla-handler.php
@@ -0,0 +1,20 @@
+<?php
+
+// ****************************************************************************
+/**
+ * Nurogames SeaClouds Casestudy sla-handler
+ *
+ * sla-handler.php
+ *
+ * @author      Christian Tismer, Nurogames GmbH
+ * 
+ * @copyright   Copyright (c) 2016, Nurogames GmbH
+ * 
+ * This is an exemplary sla-handler plugin
+ * 
+ */
+// ****************************************************************************
+
+$kEffector->result->handler = 'SLA-handler';
+
+?>
